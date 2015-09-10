89ece1d5678ac793cbfe147e9e4afc7732bbe4c9
Marshall Yount <marshall.yount@gmail.com>
2015-09-10 13:16:00 -0500

    change root route to entries#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :entries
 
-  root 'home#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/facebook', as: :login
   get '/logout', to: "sessions#destroy", as: :logout
 
+  root 'entries#index'
 end
