917ad7eec02ff7eacdadfe66d88f99f5929fcd9b
Marshall Yount <marshall.yount@gmail.com>
2015-09-12 10:13:36 -0500

    rails g model post user:references posted_at:datetime

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910003924) do
+ActiveRecord::Schema.define(version: 20150912151258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20150910003924) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "posted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "fb_id"
@@ -32,4 +41,5 @@ ActiveRecord::Schema.define(version: 20150910003924) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
