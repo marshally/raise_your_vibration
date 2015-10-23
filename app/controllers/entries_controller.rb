b49cad3e5315fad1b32b80ffe3332a5e04adedd5
Marshall Yount <marshall.yount@gmail.com>
2015-10-22 20:04:07 -0700

    use nested resources properly

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,21 +2,22 @@ require 'entry'
 
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :set_post
 
   # GET /entries
   def index
-    @entries = Entry.all
-    @entry = Entry.new
+    @entries = @post.entries
+    @entry = @post.entries.new
   end
 
   # POST /entries
   def create
-    @entry = Entry.new(entry_params)
+    @entry = @post.entries.new(entry_params)
 
     respond_to do |format|
       if @entry.save
         @entries = Entry.all
-        format.html { redirect_to :entries }
+        format.html { redirect_to @post }
       else
         format.html { render :new }
       end
@@ -25,13 +26,17 @@ class EntriesController < ApplicationController
 
   def destroy
     @entry.destroy
-    redirect_to :entries
+    redirect_to @post
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_entry
       @entry = Entry.find(params[:id])
+    end
+
+    def set_post
+      @post  = Post.find(params[:post_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
