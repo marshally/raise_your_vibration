af2d8db036b5e9605a58895f308587b1c4109feb
Marshall Yount <myount@twilio.com>
2016-01-08 14:44:51 -0600

    add body to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912155432) do
+ActiveRecord::Schema.define(version: 20160108204314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150912155432) do
     t.datetime "posted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "body"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
