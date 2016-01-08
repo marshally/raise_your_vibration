c60081d71892d6bcfc1005e687551323552dff09
Marshall Yount <myount@twilio.com>
2016-01-08 14:45:28 -0600

    obtain Facebook permissions for posting to groups

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+    scope: "user_managed_groups,publish_actions,user_posts", :display => 'popup'
 end
