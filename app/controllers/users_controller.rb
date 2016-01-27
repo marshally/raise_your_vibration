da0cc3e1bc3daa2d505543d3a6d6d8f8a7c07a2e
Marshall Yount <myount@twilio.com>
2016-01-26 21:52:22 -0600

    require phone numbers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+
+    if @user.phone_number.empty?
+      @user.errors[:phone_number] << "has not been set"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(
+        params.require(:user).permit(:phone_number)
+    )
+    redirect_to newest_posts_path(@user)
+  end
+end
